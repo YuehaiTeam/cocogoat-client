1683a83a6b398cd7e89ac1fc081f21700330affc
xytoki <7547189+xytoki@users.noreply.github.com>
2021-05-12 10:52:47 +0800

    🐳 chore: add error reporting for workers

--- src/Background/Workers/index.ts
+++ src/Background/Workers/index.ts
@@ -1,9 +1,18 @@
-import { IConfig } from '@/typings/config'
+import { IConfig, EBuild } from '@/typings/config'
 import { workerData } from 'worker_threads'
 import { ocrWorkerInit } from './ocr'
 import { logHook } from '../Utils/LogHook'
+import * as Sentry from '@sentry/electron'
 logHook()
 export const config: IConfig = workerData.config
+if (config.options.sendErrorReports && process.env.NODE_ENV !== 'development') {
+    Sentry.init({
+        dsn: process.env.VUE_APP_SENTRY,
+        environment: config.build ? config.build.type : 'DEV',
+        release: config.build && config.build.type === EBuild.REL ? config.version : 'dev',
+    })
+}
+console.log('Worker started :', workerData.worker)
 console.log('Worker started')
 switch (workerData.worker) {
     case 'ppocr':
